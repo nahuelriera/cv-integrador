e89ec78423c4cc32afcc2f4eb096e1ad438e5e3a
nahuelriera <nahuelriera1998@gmail.com>
2023-01-31 01:41:11 -0300

    cv-integrador

--- CV_INTEGRADOR/cv.js
+++ CV_INTEGRADOR/cv.js
@@ -1,7 +1,19 @@
+document.getElementById("titulo").addEventListener("mouseover", function() {
+    document.getElementById("titulo").style.textDecoration = "underline"
+})
+
+document.getElementById("titulo").addEventListener("mouseout", function() {
+    document.getElementById("titulo").style.textDecoration = "none"
+})
+
 document.getElementById("boton-educacion").addEventListener("click", function() {
     document.getElementById("educacion").style.display = "block"
 })
 
 document.getElementById("boton-laboral").addEventListener("click", function() {
     document.getElementById("laboral").style.display = "block"
+})
+
+document.getElementById("boton-referencias").addEventListener("click", function() {
+    document.getElementById("referencias").style.display = "block"
 })
