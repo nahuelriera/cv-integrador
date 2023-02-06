a06826b51087ad0e8b435b5c21738171977a5fd8
nahuelriera <nahuelriera1998@gmail.com>
2023-02-06 01:26:31 -0300

    cv integrador

--- CV_INTEGRADOR/cv.js
+++ CV_INTEGRADOR/cv.js
@@ -1,19 +1,43 @@
-document.getElementById("titulo").addEventListener("mouseover", function() {
-    document.getElementById("titulo").style.textDecoration = "underline"
-})
+function cargaLaPagina(elementoCargado, funcion) {
+    document.getElementById(elementoCargado).addEventListener("load", funcion)
+}
 
-document.getElementById("titulo").addEventListener("mouseout", function() {
-    document.getElementById("titulo").style.textDecoration = "none"
+function subrayado(elementoSubrayado) {
+    document.getElementById(elementoSubrayado).addEventListener("mouseover", function() {
+    document.getElementById(elementoSubrayado).style.textDecoration = "underline"
+    })
+    document.getElementById(elementoSubrayado).addEventListener("mouseout", function() {
+    document.getElementById(elementoSubrayado).style.textDecoration = "none"
 })
+}
 
-document.getElementById("boton-educacion").addEventListener("click", function() {
-    document.getElementById("educacion").style.display = "block"
-})
+function eventoClickYAparicion(elemento1, elemento2) {
+    document.getElementById(elemento1).addEventListener("click", function() {
+        document.getElementById(elemento2).style.display = "block"
+    })
+}
 
-document.getElementById("boton-laboral").addEventListener("click", function() {
-    document.getElementById("laboral").style.display = "block"
-})
+function respuesta(elemento1, elemento2, elemento3) {
+    document.getElementById(elemento1).addEventListener("click", function() {
+        document.getElementById(elemento2).style.display = "block"
+        document.getElementById(elemento3).style.display = "none"
+    })
+}
 
-document.getElementById("boton-referencias").addEventListener("click", function() {
-    document.getElementById("referencias").style.display = "block"
-})
+cargaLaPagina("cookies", alert("Este sitio web contiene cookies que van a mejorar su experiencia como usuario"))
+
+subrayado("titulo")
+
+subrayado("nombre")
+
+eventoClickYAparicion("boton-educacion", "educacion")
+
+eventoClickYAparicion("boton-laboral", "laboral");
+
+eventoClickYAparicion("boton-referencias", "referencias");
+
+respuesta("responde-si", "aceptacion", "rechazo");
+
+respuesta("responde-no", "rechazo", "aceptacion");
+
+document.getElementsByClassName("botones-mensajes").addEventListener("submit", alert("Su respuesta sera enviada a Manuel Romero, muchas gracias."))
